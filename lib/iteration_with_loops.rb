1f1f61e67dc32016d640dfad4e879e8f6beda34b
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-23 19:01:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while row_index < src.count do
   element_index = 0
   lowest_num = ""
   while element_index < src[row_index].count do
-  if src[row_index][element_index].min < lowest_num.min
+  if src[row_index].min < lowest_num.min
       lowest_num = src[row_index][element_index]
   end
     element_index += 1
