94cae328408d89e3d8c9ac3e83a2362fede72c50
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-23 18:27:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,9 +4,14 @@ def find_min_in_nested_arrays(src)
   
   outer_results = []
 row_index = 0
-while row_index < spice_rack.count do
+while row_index < src.count do
   element_index = 0
   longest_string_element = ""
-  while element_index < spice_rack[row_index].count do
+  while element_index < src[row_index].count do
+  if src[row_index][element_index].length > longest_string_element.length
+      longest_string_element = src[row_index][element_index]
+  end
+    element_index += 1
+  end
 
 end
